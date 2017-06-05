c05e9dcb3fd7b067d0aa45ff8804bd23143eaac7
Emile Kroeger <flammifer@gmail.com>
2017-06-05 23:56:05 +0200

    have colors based on families. Still not perfect.

--- app/scripts/controllers/wordtree.js
+++ app/scripts/controllers/wordtree.js
@@ -36,8 +36,40 @@ angular.module('cognatreeApp')
         }
       }
     }
+    var FAMCOLORS = {
+      'Albanian': '#ffffff',
+      'Anatolian': '#ffffff',
+      'Armenian': '#ffffff',
+      'Celtic / Brittonic': '#ffffff',
+      'Celtic / Continental': '#ffffff',
+      'Celtic / Goidelic': '#ffffff',
+      'Germanic / East': '#00ffff',
+      'Germanic / North': '#00aaff',
+      'Germanic / North / East': '#3399ff',
+      'Germanic / North / West': '#6688ff',
+      'Germanic / West / Anglo-Frisian': '#0022ff',
+      'Germanic / West / High German': '#2211ff',
+      'Germanic / West / Low Franconian': '#4400ff',
+      'Greek': '#ffffff',
+      'Indo-Iranian': '#ffff00',
+      'Oriya': '#ffffff',
+      'Osco-Umbrian': '#ffffff',
+      'Romance': '#ff00ff',
+      'Romance / Eastern': '#ff00ff',
+      'Romance / Gallic / Occitan': '#ff00ff',
+      'Romance / Gallic / Oil': '#ff00ff',
+      'Romance / Gallic / Rhaetian': '#ff00ff',
+      'Romance / Iberian': '#ff00ff',
+      'Romance / Italo-Dalmatian': '#ff00ff',
+      'Romance / Sardinian': '#ff00ff',
+      'Slavic': '#00ff00',
+      'Slavic / Balto-Slavic': '#00ff00',
+      'Slavic / West': '#00ff00',
+      }
+
     return {
       onReady: onReady,
+      colors: FAMCOLORS,
     }
   })
   .controller('WordtreeCtrl', function ($scope, $routeParams, $http, sLangInfo) {
@@ -100,26 +132,37 @@ angular.module('cognatreeApp')
       console.debug(familydata);
       $scope.meaning = familydata._MEANING;
       delete familydata._MEANING;
-      BRANCHES.forEach(function(branchLanguages) {
-        var branch = [];
-        branchLanguages.forEach(function(lang) {
-          var lang_val = familydata[lang];
-          if (lang_val) {
-            var entry = {
-              lang: lang,
-              writing: lang_val[0],
-              pronunciation: lang_val[1],
-            };
-            branch.push(entry);
-            delete familydata[lang];
+      sLangInfo.onReady(function(langInfo) {
+        BRANCHES.forEach(function(branchLanguages) {
+          var branch = [];
+          branchLanguages.forEach(function(lang) {
+            var lang_val = familydata[lang];
+            var family = '?';
+            var color = 'lightgrey';
+            if (langInfo[lang]) {
+              family = langInfo[lang]['Family'];
+              color = sLangInfo.colors[family];
+            }
+            if (lang_val) {
+              var entry = {
+                family: family,
+                color: color,
+                lang: lang,
+                writing: lang_val[0],
+                pronunciation: lang_val[1],
+              };
+              branch.push(entry);
+              delete familydata[lang];
+            }
+          });
+          if (branch.length) {
+            $scope.branches.push(branch);
           }
         });
-        if (branch.length) {
-          $scope.branches.push(branch);
-        }
+        $scope.familydata = familydata;
+        window.familydata = familydata;
       });
-      $scope.familydata = familydata;
-      window.familydata = familydata;
+      
     }).
     error(function(data, status) {
       console.debug('error');
