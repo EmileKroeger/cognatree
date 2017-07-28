1b97ca54f422a2b1f0fefa620b3d205a8b5906e2
Emile Kroeger <flammifer@gmail.com>
2017-07-28 04:26:08 +0200

    get sankey diagram to work (didn't work with bower, had to integrate it myself)

--- app/scripts/directives/flowgraph.js
+++ app/scripts/directives/flowgraph.js
@@ -13,6 +13,85 @@ angular.module('cognatreeApp')
       restrict: 'E',
       link: function(scope, element, attrs) {
         d3Service.d3().then(function(d3) {
+      
+      var width = 800;
+      var height = 800;
+
+      var svg = d3.select(element[0]).append('svg')
+        .attr("width", width)
+        .attr("height", height)
+      //var svg = d3.select("svg"),
+      //    width = +svg.attr("width"),
+      //    height = +svg.attr("height");
+
+      var formatNumber = d3.format(",.0f"),
+          format = function(d) { return formatNumber(d) + " TWh"; },
+          color = d3.scaleOrdinal(d3.schemeCategory10);
+
+      var sankey = d3.sankey()
+          .nodeWidth(15)
+          .nodePadding(10)
+          .extent([[1, 1], [width - 1, height - 6]]);
+      
+          window.sankey = sankey;
+
+      var link = svg.append("g")
+          .attr("class", "links")
+          .attr("fill", "none")
+          .attr("stroke", "#000")
+          .attr("stroke-opacity", 0.2)
+        .selectAll("path");
+
+      var node = svg.append("g")
+          .attr("class", "nodes")
+          .attr("font-family", "sans-serif")
+          .attr("font-size", 10)
+        .selectAll("g");
+
+      d3.json("data/energy.json", function(error, energy) {
+        if (error) throw error;
+
+        sankey(energy);
+
+        link = link
+          .data(energy.links)
+          .enter().append("path")
+            .attr("d", d3.sankeyLinkHorizontal())
+            .attr("stroke-width", function(d) { return Math.max(1, d.width); });
+
+        link.append("title")
+            .text(function(d) { return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
+
+        node = node
+          .data(energy.nodes)
+          .enter().append("g");
+
+        node.append("rect")
+            .attr("x", function(d) { return d.x0; })
+            .attr("y", function(d) { return d.y0; })
+            .attr("height", function(d) { return d.y1 - d.y0; })
+            .attr("width", function(d) { return d.x1 - d.x0; })
+            .attr("fill", function(d) { return color(d.name.replace(/ .*/, "")); })
+            .attr("stroke", "#000");
+
+        node.append("text")
+            .attr("x", function(d) { return d.x0 - 6; })
+            .attr("y", function(d) { return (d.y1 + d.y0) / 2; })
+            .attr("dy", "0.35em")
+            .attr("text-anchor", "end")
+            .text(function(d) { return d.name; })
+          .filter(function(d) { return d.x0 < width / 2; })
+            .attr("x", function(d) { return d.x1 + 6; })
+            .attr("text-anchor", "start");
+
+        node.append("title")
+            .text(function(d) { return d.name + "\n" + format(d.value); });
+      });
+
+
+
+          
+          /*
 
           var margin = {top: 20, right: 20, bottom: 30, left: 50},
             width = 600 - margin.left - margin.right,
@@ -73,6 +152,7 @@ angular.module('cognatreeApp')
             .attr("r", function(d) { return 10; })
             .attr("cx", function(d, i) { return x(d.date); })
             .attr("cy", function(d) { return y(d.close); });
+          */
             
         });
       },
